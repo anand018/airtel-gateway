0220024950ee14fbdf2efc951fda7c298de7878c
Anand Pratap Singh <42322585+anand018@users.noreply.github.com>
2020-03-18 14:12:32 +0530

    Add files via upload

---
+++ src/main/java/com/airtel/gateway/SmartGateway/SmartGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.airtel.gateway.SmartGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airtel/gateway/SmartGateway/SmartGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtel.gateway.SmartGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
